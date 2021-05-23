d96b5690d3d407739cc8a8630468c7d9d6a13fd5
xry <362438997@qq.com>
2021-05-24 00:25:24 +0800

    init

--- mycmdb/settings.py
+++ mycmdb/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'mycmdb.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cmdb',  # 数据库名
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'root00',
+        'HOST': '127.0.0.1',
     }
 }
 
